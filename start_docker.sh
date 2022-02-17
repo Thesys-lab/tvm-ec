7149779ac6c37de590b1dde35f509c4e220341b7
Jack Kosaian <jkosaian@andrew.cmu.edu>
2022-02-17 19:23:46 +0000

    [setup] Fix in start_docker.sh script

--- start_docker.sh
+++ start_docker.sh
@@ -5,6 +5,7 @@ IMAGE_NAME=ec-tvm
 build_container() {
   cd .devcontainer
   docker build . -f Dockerfile-interactive -t $IMAGE_NAME
+  cd ..
 }
 
 if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
