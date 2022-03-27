10a9a307d32b4098a2318aa501d414ab61fcc71e
JiyuuuHuuu <hu_jiyu@icloud.com>
2022-03-27 04:39:53 -0400

    adding mkl referecing benchmark, the input is not randomized

---
+++ mkl/matmul/src/simple_gemm.c
@@ -0,0 +1,99 @@
+/* C source code is found in dgemm_example.c */
+
+#define min(x,y) (((x) < (y)) ? (x) : (y))
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mkl.h"
+
+const int m = 1024;
+const int k = 1024;
+const int n = 1024;
+
+
+int main()
+{
+    double *A, *B, *C;
+    int i, j;
+    double alpha, beta;
+
+    printf ("\n This example computes real matrix C=alpha*A*B+beta*C using \n"
+            " Intel(R) MKL function dgemm, where A, B, and  C are matrices and \n"
+            " alpha and beta are double precision scalars\n\n");
+
+    printf (" Initializing data for matrix multiplication C=A*B for matrix \n"
+            " A(%ix%i) and matrix B(%ix%i)\n\n", m, k, k, n);
+    alpha = 1.0; beta = 0.0;
+
+    printf (" Allocating memory for matrices aligned on 64-byte boundary for better \n"
+            " performance \n\n");
+    A = (double *)mkl_malloc( m*k*sizeof( double ), 64 );
+    B = (double *)mkl_malloc( k*n*sizeof( double ), 64 );
+    C = (double *)mkl_malloc( m*n*sizeof( double ), 64 );
+    if (A == NULL || B == NULL || C == NULL) {
+      printf( "\n ERROR: Can't allocate memory for matrices. Aborting... \n\n");
+      mkl_free(A);
+      mkl_free(B);
+      mkl_free(C);
+      return 1;
+    }
+
+    printf (" Intializing matrix data \n\n");
+    for (i = 0; i < (m*k); i++) {
+        A[i] = (double)(i+1);
+    }
+
+    for (i = 0; i < (k*n); i++) {
+        B[i] = (double)(-i-1);
+    }
+
+    for (i = 0; i < (m*n); i++) {
+        C[i] = 0.0;
+    }
+
+    printf (" Computing matrix product using Intel(R) MKL dgemm function via CBLAS interface \n\n");
+    double start = dsecnd();
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 
+                m, n, k, alpha, A, k, B, n, beta, C, n);
+    double end = dsecnd();
+    printf ("\n Computations completed.\n\n");
+
+    printf (" Top left corner of matrix A: \n");
+    for (i=0; i<min(m,6); i++) {
+      for (j=0; j<min(k,6); j++) {
+        printf ("%12.0f", A[j+i*k]);
+      }
+      printf ("\n");
+    }
+
+    printf ("\n Top left corner of matrix B: \n");
+    for (i=0; i<min(k,6); i++) {
+      for (j=0; j<min(n,6); j++) {
+        printf ("%12.0f", B[j+i*n]);
+      }
+      printf ("\n");
+    }
+    
+    printf ("\n Top left corner of matrix C: \n");
+    for (i=0; i<min(m,6); i++) {
+      for (j=0; j<min(n,6); j++) {
+        printf ("%12.5G", C[j+i*n]);
+      }
+      printf ("\n");
+    }
+
+    printf ("\n Deallocating memory \n\n");
+    mkl_free(A);
+    mkl_free(B);
+    mkl_free(C);
+
+    printf (" Example completed. \n\n");
+    
+    printf ("\nStart time: %f s\n", start);
+    printf ("End time: %f s\n\n", end);
+    double duration = end - start;
+    double size = (double)m*k*sizeof( double ) + (double)k*n*sizeof( double ) + (double)m*n*sizeof( double );
+    double bandwidth = size/duration/(1024*1024);
+    printf ("Bandwidth: %f MB/s\n\n", bandwidth);
+    return 0;
+}
