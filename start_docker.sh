e650a6541701cee7225bad950f436b324b193452
JiyuuuHuuu <hu_jiyu@icloud.com>
2022-04-14 02:39:51 -0400

    let container start in background

--- start_docker.sh
+++ start_docker.sh
@@ -12,4 +12,4 @@ if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
   build_container
 fi
 
-docker run -it --rm -v $(pwd):/home/tvm $IMAGE_NAME
+docker run -it -d -v $(pwd):/home/tvm $IMAGE_NAME
