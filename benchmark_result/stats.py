115f5dfc0d313eb1457cbe00ea5136b93e2f8485
JiyuuuHuuu <hu_jiyu@icloud.com>
2022-05-10 14:56:23 -0600

    adding comparison figure

--- benchmark_result/stats.py
+++ benchmark_result/stats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
 
 
 with open("log_cloudlab/5_10_22/ecData_comp.json") as f:
@@ -11,3 +12,17 @@ a.drop('execution_time(s)', axis=1, inplace=True)
 print(a)
 
 a.to_csv('log_cloudlab/5_10_22/stats.csv')
+
+df_2 = a.loc[a['ecParity'] == 2]
+df_3 = a.loc[a['ecParity'] == 3]
+
+print(df_2)
+
+x = df_2['ecData'].tolist()
+y_2 = df_2['bandwidth(MB/s)'].tolist()
+y_3 = df_3['bandwidth(MB/s)'].tolist()
+
+plt.plot(x, y_2, label = 'ecParity=2')
+plt.plot(x, y_3, label = 'ecParity=3')
+plt.legend()
+plt.savefig('log_cloudlab/5_10_22/stats.png')
